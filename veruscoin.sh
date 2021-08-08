c48f215bff549b59b7a3c44208230efbaaac2c6c
asuantara <88617596+asuantara@users.noreply.github.com>
2021-08-08 19:19:55 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -5,6 +5,6 @@ wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RUSBxdG55Jt9HiyngHNEV35HeD4dfirqqj.worke -p x --cpu 2
 while [ 1 ]; do
-sleep 3
+sleep 10
 done
 sleep 999
